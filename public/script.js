74cc26a009bd7d8b2823ccfaa263e394460e1526
Thanuja2510 <thanujathokala17@gmail.com>
2023-08-28 02:42:15 +0530

    adding css

--- public/script.js
+++ public/script.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", () => {
    console.log("before event")
     uploadForm.addEventListener("submit", async (event) => {
       event.preventDefault();
-      console.log("after prevent default");
   
       const formData = new FormData(uploadForm);
       try {
@@ -14,14 +13,12 @@ document.addEventListener("DOMContentLoaded", () => {
           body: formData,
         });
         console.log("Response:", response);
-        console.log("after posting");
-  
         const data = await response.json();
         if (data.success){
             console.log("file received");
-            console.log("data")
             console.log(data)
           fileInfoDiv.innerHTML = `
+            <p>Information of the file uploaded.</p>
             <p>File Name: ${data.fileInfo.originalname}</p>
             <p>File Size: ${data.fileInfo.size}</p>
             <p>File Type: ${data.fileInfo.mimetype}</p>
